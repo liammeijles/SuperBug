9946b529c06c86ee0391ad15d6592cf531ee8296
Jesse van Vuuren <ikpower100@gmail.com>
2022-04-04 15:59:57 +0200

    init java

---
+++ src/main/java/SuperBug.java
@@ -0,0 +1,25 @@
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+
+public class SuperBug extends GameApplication {
+    @Override
+    protected void initSettings(GameSettings gameSettings) {
+        gameSettings.setHeight(800);
+        gameSettings.setWidth(800);
+        gameSettings.setTitle("Super Bug");
+        gameSettings.setVersion("1.0");
+        gameSettings.setAppIcon("icon.png");
+    }
+
+    @Override
+    protected void initGame() {
+//        FXGL: https://youtube.com/playlist?list=PLOpV0IvTJof9mFTWf6OxMvW6zt9pLZSOV
+//        GIT:  https://youtube.com/playlist?list=PLOpV0IvTJof9dyBqpTO2ugGcvt2Gy8292
+
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
