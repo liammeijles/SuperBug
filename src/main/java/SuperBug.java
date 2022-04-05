85fb5a5c41768222e363af53623b5581c054bb7e
Jesse van Vuuren <ikpower100@gmail.com>
2022-04-05 09:22:50 +0200

    player

--- src/main/java/SuperBug.java
+++ src/main/java/SuperBug.java
@@ -1,7 +1,14 @@
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.dsl.FXGL;
+import com.almasb.fxgl.entity.Entity;
+import javafx.scene.input.KeyCode;
+
 
 public class SuperBug extends GameApplication {
+
+    Entity player;
+
     @Override
     protected void initSettings(GameSettings gameSettings) {
         gameSettings.setHeight(800);
@@ -16,7 +23,32 @@ public class SuperBug extends GameApplication {
 //        FXGL: https://youtube.com/playlist?list=PLOpV0IvTJof9mFTWf6OxMvW6zt9pLZSOV
 //        GIT:  https://youtube.com/playlist?list=PLOpV0IvTJof9dyBqpTO2ugGcvt2Gy8292
 
+        player = FXGL.entityBuilder()
+                .at(400, 400)
+                .view("bug01.png")
+                .scale(0.1,0.1)
+                .buildAndAttach();
+    }
 
+    @Override
+    protected void initInput() {
+        FXGL.onKey(KeyCode.W, () -> {
+            player.translateY(-5);
+            player.setRotation(0);
+        });
+        FXGL.onKey(KeyCode.A, () -> {
+            player.translateX(-5);
+            player.setRotation(-90);
+
+        });
+        FXGL.onKey(KeyCode.S, () -> {
+            player.translateY(5);
+            player.setRotation(180);
+        });
+        FXGL.onKey(KeyCode.D, () -> {
+            player.translateX(5);
+            player.setRotation(90);
+        });
     }
 
     public static void main(String[] args) {
