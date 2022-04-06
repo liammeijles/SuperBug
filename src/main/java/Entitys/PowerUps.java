89c4a6d6149a90de6599831710c72793b89dea2c
Jesse van Vuuren <32089540+JessevanVuuren@users.noreply.github.com>
2022-04-06 13:14:37 +0200

    power up shotgun, exta bullet

--- src/main/java/Entitys/PowerUps.java
+++ src/main/java/Entitys/PowerUps.java
@@ -4,6 +4,7 @@ public enum PowerUps {
     // normal bug type = bug01.png
     FAST_SHOOTING("bug07.png"),
     MEGA_HEALTH("bug08.png"),
+    SHOTGUN_HEALTH("bug05.png"),
     QUAD_ATTACK("bug06.png");
 
     private final String bugType;
