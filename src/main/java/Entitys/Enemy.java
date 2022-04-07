d23d641cf05ab28393d3535b20724f2d6dcd7fc6
MusingOwl <lantingasil04@gmail.com>
2022-04-07 14:21:42 +0200

    Enemy type waves
    - Made different enemy types
    - Made it possible to trigger different enemy types for waves

--- src/main/java/Entitys/Enemy.java
+++ src/main/java/Entitys/Enemy.java
@@ -9,6 +9,8 @@ import javafx.geometry.Point2D;
 import javafx.geometry.Rectangle2D;
 
 
+import java.util.Random;
+
 import static Entitys.EntityTypes.PLAYER;
 import static com.almasb.fxgl.dsl.FXGL.*;
 
@@ -16,29 +18,59 @@ public class Enemy{
 
     HealthIntComponent health;
 
+
     public Entity spawnEnemy(SpawnData data) {
         health = new HealthIntComponent(1);
 
+        double X = 0;
+        double Y = 0;
         double randX = (Math.random() * 2) - 1;
         double randY = (Math.random() * 1) - 1;
         double randSpeed = (Math.random() * 60) + 60;
-        double playX = FXGL.getGameWorld().getSingleton(PLAYER).getCenter().getX()-400;
+        double playX = FXGL.getGameWorld().getSingleton(PLAYER).getCenter().getX() - 400;
         double playY = FXGL.getGameWorld().getSingleton(PLAYER).getCenter().getY();
+        String photo = "enemy01.png";
+        Component test = new ProjectileComponent(new Point2D(-randX, -randY), randSpeed);
+        EnemyTypes enemyType = EnemyTypes.getEnemytypes(null);
 
+
+
+        switch (enemyType) {
+            case C:
+                X = 400;
+                Y = 0;
+                test = new ProjectileComponent(new Point2D(-randX, -randY), randSpeed);
+                break;
+            case JQ:
+                X = 400;
+                Y = 0;
+                test = new ProjectileComponent(new Point2D(playX, playY), randSpeed);
+
+
+                break;
+
+            case JS:
+                X = Math.random() * 800;
+                Y = Math.random() * 800;
+                test = new RandomMoveComponent(new Rectangle2D(0, 0, getAppHeight(), getAppWidth()), 50);
+                break;
+        }
         return entityBuilder(data)
                 .type(EntityTypes.ENEMY)
-                .viewWithBBox("enemy01.png")
+                .viewWithBBox(photo)
                 .with(health)
-                .at(400,0)
+                .at(X, Y)
                 .with(new OffscreenCleanComponent())
-                //.at(Math.random() * 800, Math.random() * 800)
-                .with(new ProjectileComponent(new Point2D(-randX,-randY), randSpeed))
+                .with (test)
+                //.with(new ProjectileComponent(new Point2D(-randX, -randY), randSpeed))
                 //.with (new ProjectileComponent(new Point2D(playX, playY), randSpeed))
                 //.with(new RandomMoveComponent(new Rectangle2D(0, 0, getAppHeight(), getAppWidth()), 50))
                 //.with(new FollowComponent())
                 .collidable()
                 .build();
-    }
+
+        }
+
 
     public void dmgEnemy(int dmg) {
         this.health.damage(dmg);
--- src/main/java/SpawnEnemyWave.java
+++ src/main/java/SpawnEnemyWave.java
@@ -1,9 +1,9 @@
+import Entitys.EnemyTypes;
 import Entitys.EntityTypes;
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
 import javafx.util.Duration;
 
-import java.util.Dictionary;
 import java.util.List;
 
 import static com.almasb.fxgl.dsl.FXGL.*;
@@ -12,37 +12,48 @@ import static com.almasb.fxgl.dsl.FXGL.*;
 public class SpawnEnemyWave {
 
     private boolean waveSpawned = false;
-    private int levelCount = 4;
+    private int levelCount = 0;
+    private int enemyLimit = 0;
+    private double waveDelay = 1.0;
+    public EnemyTypes enemyType = EnemyTypes.C;
+
 
     public void waveManager() {
         FXGL.getGameTimer().runAtInterval(() -> {
             List<Entity> list = getGameWorld().getEntitiesByType(EntityTypes.ENEMY);
             System.out.println("Spawn wave");
-            System.out.println(list.size());
             System.out.println(levelCount);
-            if (list.size() < 1 && !waveSpawned) {
+
+            if (list.size() <= enemyLimit  && !waveSpawned) {
                 waveSpawned = true;
-                FXGL.getGameTimer().runOnceAfter(() -> {
-                    if (levelCount < 5) {
-                        spawnSimpleWave(1, 1000);}
-                    else {
-                        spawnSimpleWave(100, 10000);}
-                    levelCount++;
-                    waveSpawned = false;
-                }, Duration.seconds(0));
+                if (levelCount < 5) {
+                    EnemyTypes.getEnemytypes(EnemyTypes.C);
+                    spawnSimpleWave(1);
+                    enemyLimit = 5;
+
+                }
+
+                else {
+                    EnemyTypes.getEnemytypes(EnemyTypes.JQ);
+                    spawnSimpleWave(3);
+                    enemyLimit = 11;
+                    waveDelay = 3000.0;
+
+                }
+                levelCount++;
             }
-        }, Duration.seconds(1.1));
+        }, Duration.seconds(waveDelay));
 
     }
 
-    public void spawnSimpleWave(int amount, int seconds) {
-        for (int i = 0; i < amount; i++) {
+    public void spawnSimpleWave(int amount) {
+        FXGL.getGameTimer().runOnceAfter(() -> {
+            for (int i = 0; i < amount; i++) {
+                spawn("enemy", 0, 0);
+                waveSpawned = false;
 
-            FXGL.getGameTimer().runOnceAfter(() -> {
-                spawn("enemy", 0,0);
-            }, Duration.millis(seconds));
-
-        }
+            }
+        }, Duration.millis(waveDelay));
     }
 
 }
